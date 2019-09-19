dd009b986cea1c510358f2cb9b9247f11650c8bc
erik suserud <eriksuserud@gmail.com>
2019-09-19 11:41:46 +0200

    steg1 med try&catch

--- Steg1/Program.cs
+++ Steg1/Program.cs
@@ -6,22 +6,21 @@ namespace Steg1
     {
         static void Main(string[] args)
         {
+
+       
             
-            int num1, num2, num3, num4, num5;
+           try{
             System.Console.Write("Mata in tal 1: ");
-            num1 = Convert.ToInt32(Console.ReadLine());   
+            int num1 = Convert.ToInt32(Console.ReadLine());   
             System.Console.Write("Mata in tal 2: ");
-            num2 = Convert.ToInt32(Console.ReadLine()); 
+            int num2 = Convert.ToInt32(Console.ReadLine()); 
             System.Console.Write("Mata in tal 3: ");
-            num3 = Convert.ToInt32(Console.ReadLine()); 
+            int num3 = Convert.ToInt32(Console.ReadLine()); 
             System.Console.Write("Mata in tal 4: ");
-            num4 = Convert.ToInt32(Console.ReadLine()); 
+            int num4 = Convert.ToInt32(Console.ReadLine()); 
             System.Console.Write("Mata in tal 5: ");
-            num5 = Convert.ToInt32(Console.ReadLine());
-
-          
-
-
+            int num5 = Convert.ToInt32(Console.ReadLine());
+         
 
             int[] nums = new int[5];
             nums[0] = num1;
@@ -29,7 +28,9 @@ namespace Steg1
             nums[2] = num3;
             nums[3] = num4;
             nums[4] = num5;
-        
+           
+         
+            
            int sum = 0;
 
            for(int i = 0; i<nums.Length; i++)
@@ -37,11 +38,18 @@ namespace Steg1
             {
                 sum = sum +nums[i];
             }
-            System.Console.WriteLine("Summan av talen är {0}", sum);
+       
+      
+            
+            System.Console.WriteLine("Summan av talen är: {0}", sum);
+           }
+           catch
+           {
+               System.Console.WriteLine("Du måste mata in ett heltal!!");
+           } 
+            
 
-
-
-
+            
             
             
             
