8632281c89f16aa24a55ab02cac856f78b1d21cf
erik suserud <eriksuserud@gmail.com>
2019-09-23 09:17:49 +0200

    steg2-klar

---
+++ Steg2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Steg2
+{
+    class Program
+    {
+        /*
+        Steg 2 - Skapa en string array med fem element. Användaren får mata in fem namn
+        via en for-loop. Programmet skriver sedan ut dessa namn med hjälp av en for-loop. 
+         */
+        static void Main(string[] args)
+        {
+            string[] names = new string[5];
+            
+
+
+            
+            for(int i = 0; i < names.Length; i++)
+            {
+                System.Console.Write("Mata in ett namn: ");
+                string name = Console.ReadLine();
+                names[i] += name;
+            }
+            
+            for(int i = 0; i < names.Length; i++)  
+            {
+                System.Console.WriteLine(names[i]);
+            }
+        }
+     
+    
+
+       
+    } 
+}   
